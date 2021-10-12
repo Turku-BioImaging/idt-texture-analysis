254305872e44bfeef4dbe7c8a9d1df7c53fa946c
Junel Solis <junel.solis@gmail.com>
2021-10-12 15:42:50 +0300

    pseudocode WIP

---
+++ measure.py
@@ -0,0 +1,10 @@
+# expect image data within data directory
+# expect masks to have "mask-" prepended to its corresponding image filename
+
+# rescale intensity image to uint8
+
+# get region properties using scikit image, which gives the intensity image within ROI
+
+# get haralick features using mahotas
+
+# tabulate and export to csv
