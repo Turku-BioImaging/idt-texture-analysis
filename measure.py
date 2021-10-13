4330e38cc12acf2804a79eea0a5467990ad99fc6
Junel Solis <junel.solis@gmail.com>
2021-10-13 08:19:13 +0300

    add banner, assertions, and print completion message

--- measure.py
+++ measure.py
@@ -1,21 +1,45 @@
 import os
 import pandas as pd
+import numpy as np
 from glob import glob
+from tqdm import tqdm
 
 from skimage import io, measure
 
 import texture
 
-img_paths = sorted(set(glob('data/*')) - set(glob('data/mask-*')))
-mask_paths = sorted(glob('data/mask-*'))
+# Banner message
+print("""
+##################################################      
+--------------------------------------------------
 
+IMAGE TEXTURE ANALYSIS MODULE
+
+Turku BioImaging - Image Data Team
+    Website: https://bioimaging.fi
+    Repository: https://turku-bioimaging.github.io
+    Email: tbi-office@bioimaging.fi
+    
+--------------------------------------------------
+##################################################
+""")
+
+img_paths = sorted(glob('images/*'))
+mask_paths = sorted(glob('masks/*'))
+
+assert len(img_paths) == len(mask_paths), 'Image and mask counts do not match.'
 
 data = []
 
-for index, path in enumerate(img_paths):
+print(f"\nAnalyzing a total of [ {len(img_paths) } ] images...\n")
+
+for index, path in tqdm(enumerate(img_paths)):
 
     img = io.imread(path)
     mask = io.imread(mask_paths[index])
+    
+    assert np.ndim(img) == 2, 'Image dimensions are incorrect. 2D image expected.'
+    assert np.ndim(mask) == 2, 'Mask dimensions are incorrect. 2D mask expected.'
     
     img_name = path.replace('data/', '')
     
@@ -39,3 +63,9 @@ else:
         os.remove(path)
 
 df.to_csv('results/results.csv')
+
+print("""
+      
+Texture analysis complete. Results have been saved to the results directory.
+
+""")
