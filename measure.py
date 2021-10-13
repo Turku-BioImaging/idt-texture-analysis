c92e49fc37610553b3bda9a6cb256ed7fda34df2
Junel Solis <junel.solis@gmail.com>
2021-10-13 07:48:47 +0300

    commit main script

--- measure.py
+++ measure.py
@@ -1,54 +1,41 @@
 import os
-import numpy as np
-import mahotas
+import pandas as pd
 from glob import glob
 
 from skimage import io, measure
 
-HARALICK_FEATURES = [
-    'angular_second_moment',
-    'contrast',
-    'correlation',
-    'variance',
-    'inverse_difference_moment',
-    'sum_average',
-    'sum_variance',
-    'sum_entropy',
-    'entropy',
-    'difference_variance',
-    'difference_entropy',
-    'information_measure_1',
-    'information_measure_2',
-]
+import texture
 
-# expect images and masks within data directory
-# expect masks to have "mask-" prepended to its corresponding image filename
 img_paths = sorted(set(glob('data/*')) - set(glob('data/mask-*')))
 mask_paths = sorted(glob('data/mask-*'))
 
-img_path = img_paths[0]
-mask_path = mask_paths[0]
 
-img = io.imread(img_path)
-mask = io.imread(mask_path)
+data = []
 
-img[~mask] = 0
+for index, path in enumerate(img_paths):
 
-# get region properties using scikit image, which gives the intensity image within ROI
-props = measure.regionprops(mask, img)
-
-# get haralick features using mahotas
-features = mahotas.features.haralick(mask, distance=1)
-
-# loop through the 2D array of 4 directions * 13 features
-# get averages for each feature
-feature_averages = {}
-
-for index in range(len(HARALICK_FEATURES)):
-    image_name = img_path
-    feature_averages[HARALICK_FEATURES[index]] = np.mean([features[i][index] for i in range(4)])
+    img = io.imread(path)
+    mask = io.imread(mask_paths[index])
     
+    img_name = path.replace('data/', '')
+    
+    features = texture.haralick(img, mask)
+    features['image_filename'] = img_name
+    
+    data.append(features)
 
-print(feature_averages)
 
-# tabulate and export to csv
+# convert to pandas dataframe and export to csv
+df = pd.DataFrame(data)
+columns = list(df)
+columns.insert(0, columns.pop(columns.index('image_filename')))
+
+df = df.loc[:, columns]
+
+if os.path.isdir('results') == False:
+    os.mkdir('results')
+else:
+    for path in glob('results/*'):
+        os.remove(path)
+
+df.to_csv('results/results.csv')
