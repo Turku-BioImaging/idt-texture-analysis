c39003d51ce73c12ca3f29dc3d192c19bfc6cf14
Junel Solis <junel.solis@abo.fi>
2022-03-25 08:47:48 +0200

    allow module to work with 3d images

--- measure.py
+++ measure.py
@@ -7,98 +7,105 @@ from skimage import io, img_as_ubyte
 
 import texture
 
-# Banner message
-print(
+
+def print_banner():
+    print(
+        """
+    ##################################################      
+    --------------------------------------------------
+
+    IMAGE TEXTURE ANALYSIS MODULE
+
+    Turku BioImaging - Image Data Team
+        Website: https://bioimaging.fi
+        Repository: https://turku-bioimaging.github.io
+        Email: image-data@bioimaging.fi
+        
+    --------------------------------------------------
+    ##################################################
     """
-##################################################      
---------------------------------------------------
-
-IMAGE TEXTURE ANALYSIS MODULE
-
-Turku BioImaging - Image Data Team
-    Website: https://bioimaging.fi
-    Repository: https://turku-bioimaging.github.io
-    Email: tbi-office@bioimaging.fi
-    
---------------------------------------------------
-##################################################
-"""
-)
-
-img_paths = sorted(set(glob("data/*")) - set(glob("data/*_mask.*")))
-mask_paths = sorted(glob("data/*_mask.*"))
-
-assert len(img_paths) == len(mask_paths), "Image and mask counts do not match."
-
-data = []
-
-print(f"\nAnalyzing a total of [ {len(img_paths) } ] images...\n")
-
-for index, path in tqdm(enumerate(img_paths), total=len(img_paths)):
-
-    img = img_as_ubyte(io.imread(path))
-    mask = io.imread(mask_paths[index])
-
-    assert np.ndim(img) == 2, "Image dimensions are incorrect. 2D image expected."
-    assert np.ndim(mask) == 2, "Mask dimensions are incorrect. 2D mask expected."
-
-    img_name = path.replace("images/", "")
-
-    features = texture.haralick(img, mask, distance=5)
-    features["image_filename"] = img_name
-
-    data.append(features)
+    )
 
 
-# convert to pandas dataframe and export to csv
-df = pd.DataFrame(data)
-columns = list(df)
-columns.insert(0, columns.pop(columns.index("image_filename")))
+if __name__ == "__main__":
+    print_banner()
 
-df = df.loc[:, columns]
+    img_paths = sorted(set(glob("data/*")) - set(glob("data/*_mask.*")))
+    mask_paths = sorted(glob("data/*_mask.*"))
 
-if os.path.isdir("results") == False:
-    os.mkdir("results")
-else:
-    for path in glob("results/*"):
-        os.remove(path)
+    assert len(img_paths) == len(mask_paths), "Image and mask counts do not match."
 
-df.to_csv("results/results_5px.csv")
+    data = []
 
-# measure for 1px distance
-data = []
+    print(f"\nAnalyzing a total of [ {len(img_paths) } ] images...\n")
 
-print(f"\nAnalyzing a total of [ {len(img_paths) } ] images...\n")
+    for index, path in tqdm(enumerate(img_paths), total=len(img_paths)):
 
-for index, path in tqdm(enumerate(img_paths), total=len(img_paths)):
+        img = img_as_ubyte(io.imread(path))
+        mask = io.imread(mask_paths[index])
 
-    img = img_as_ubyte(io.imread(path))
-    mask = io.imread(mask_paths[index])
+        # assert np.ndim(img) == 2, "Image dimensions are incorrect. 2D image expected."
+        # assert np.ndim(mask) == 2, "Mask dimensions are incorrect. 2D mask expected."
+        assert np.ndim(img) == np.ndim(mask), "Image and mask dimensions do not match."
 
-    assert np.ndim(img) == 2, "Image dimensions are incorrect. 2D image expected."
-    assert np.ndim(mask) == 2, "Mask dimensions are incorrect. 2D mask expected."
+        # img_name = path.replace("images/", "")
+        img_name = os.path.basename(path)
 
-    img_name = path.replace("images/", "")
+        features = texture.haralick(img, mask, distance=5)
+        features["image_filename"] = img_name
 
-    features = texture.haralick(img, mask, distance=1)
-    features["image_filename"] = img_name
+        data.append(features)
 
-    data.append(features)
+    # convert to pandas dataframe and export to csv
+    df = pd.DataFrame(data)
+    columns = list(df)
+    columns.insert(0, columns.pop(columns.index("image_filename")))
 
+    df = df.loc[:, columns]
 
-# convert to pandas dataframe and export to csv
-df = pd.DataFrame(data)
-columns = list(df)
-columns.insert(0, columns.pop(columns.index("image_filename")))
+    if os.path.isdir("results") == False:
+        os.mkdir("results")
+    else:
+        for path in glob("results/*"):
+            os.remove(path)
 
-df = df.loc[:, columns]
+    df.to_csv("results/results_5px.csv")
 
-df.to_csv("results/results_1px.csv")
+    # measure for 1px distance
+    data = []
 
-print(
+    print(f"\nAnalyzing a total of [ {len(img_paths) } ] images...\n")
+
+    for index, path in tqdm(enumerate(img_paths), total=len(img_paths)):
+
+        img = img_as_ubyte(io.imread(path))
+        mask = io.imread(mask_paths[index])
+
+        # assert np.ndim(img) == 2, "Image dimensions are incorrect. 2D image expected."
+        # assert np.ndim(mask) == 2, "Mask dimensions are incorrect. 2D mask expected."
+        assert np.ndim(img) == np.ndim(mask), "Image and mask dimensions do not match."
+
+        # img_name = path.replace("images/", "")
+        img_name = os.path.basename(path)
+
+        features = texture.haralick(img, mask, distance=1)
+        features["image_filename"] = img_name
+
+        data.append(features)
+
+    # convert to pandas dataframe and export to csv
+    df = pd.DataFrame(data)
+    columns = list(df)
+    columns.insert(0, columns.pop(columns.index("image_filename")))
+
+    df = df.loc[:, columns]
+
+    df.to_csv("results/results_1px.csv")
+
+    print(
+        """
+
+    Texture analysis complete. Results have been saved to the results directory.
+
     """
-
-Texture analysis complete. Results have been saved to the results directory.
-
-"""
-)
+    )
