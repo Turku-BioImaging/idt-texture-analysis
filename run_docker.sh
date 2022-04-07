8a6483061a70b709c07c736563e2b6b18868ffe8
Junel Solis <junel.solis@abo.fi>
2022-04-07 07:11:26 +0300

    fix docker script

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,7 @@ mkdir -p results images masks
 chmod -R 777 results
 chmod -R 775 images masks
 
-docker run -it  -v "$(pwd)/images:/code/images" \
-    -v "$(pwd)/masks:/code/masks" \
+docker run -it  -v "$(pwd)/images:/code/data/images" \
+    -v "$(pwd)/masks:/code/data/masks" \
     -v "$(pwd)/results:/code/results" \
     ghcr.io/turku-bioimaging/idt-texture-analysis:0.1.5
