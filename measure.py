61e4d0283afef2521867706f121d2d3302580aef
Junel Solis <junel.solis@abo.fi>
2022-04-21 10:56:57 +0300

    exclude 5 px distance measurements

--- measure.py
+++ measure.py
@@ -91,25 +91,25 @@ if __name__ == "__main__":
 
     _save_data_to_file(data, "texture_5px_max")
 
-    # measure texture with 5 px distance and average intensity projection
-    print(f"\nAnalyzing {len(img_paths)} images using average intensity...\n")
-    data = []
-    for index, path in tqdm(enumerate(img_paths), total=len(img_paths)):
+    # # measure texture with 5 px distance and average intensity projection
+    # print(f"\nAnalyzing {len(img_paths)} images using average intensity...\n")
+    # data = []
+    # for index, path in tqdm(enumerate(img_paths), total=len(img_paths)):
 
-        img = img_as_ubyte(io.imread(path))
-        mask = io.imread(mask_paths[index])
+    #     img = img_as_ubyte(io.imread(path))
+    #     mask = io.imread(mask_paths[index])
 
-        features_c1, features_c2 = _get_features(img, mask, distance=5, mode="avg")
+    #     features_c1, features_c2 = _get_features(img, mask, distance=1, mode="avg")
 
-        data.append(features_c1)
-        data.append(features_c2)
+    #     data.append(features_c1)
+    #     data.append(features_c2)
 
-    _save_data_to_file(data, "texture_5px_avg")
+    # _save_data_to_file(data, "texture_1px_avg")
 
-    print(
-        """
+    # print(
+    #     """
 
-    Texture analysis complete. Results have been saved to the results directory.
+    # Texture analysis complete. Results have been saved to the results directory.
 
-    """
-    )
+    # """
+    # )
